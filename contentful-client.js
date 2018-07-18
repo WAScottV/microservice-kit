074baad9ce524cf441d089cf84de0135984fe1a6
Alex Scott <williamascott@cedarville.edu>
2018-07-18 08:22:28 -0400

    fixing bug that wasn't returning contenful client object.

--- contentful-client.js
+++ contentful-client.js
@@ -1,12 +1,24 @@
 const contentful = require('contentful');
 
+let client = null;
 const create = (spaceId, accessToken) => {
-  contentful.createClient({
+  client = contentful.createClient({
     space: spaceId,
     environment: 'master',
     accessToken: accessToken
   });
 };
 
+const getEntries = async () => {
+  if (client === null) {
+    throw new Error('Must create client first');
+  }
+  return await client.getEntries({
+    content_type: 'message',
+    select: 'sys.id,fields'
+  });
+}
+
 // initialize contenful client.
 module.exports.createClient = create;
+module.exports.getEntries = getEntries;
--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const contentful = require('./contentful-client');
 const http = require('http');
 const { PORT = 3000 } = process.env;
 
-let client;
-
 const main = (cb, spaceId, accessToken) => {
   // initialize Contenful client.
-  client = contentful.createClient(spaceId, accessToken);
+  contentful.createClient(spaceId, accessToken);
   http.createServer(async (req, res) => {
       let body = '';
     
@@ -30,79 +28,76 @@ const main = (cb, spaceId, accessToken) => {
       });
       
   }).listen(PORT);
-};
 
-const processRequest = async (obj) => {
-  return new Promise (function(resolve, reject) {
-    try {
-      let result = {};
-  
-      // initialize object for interrogating input.
-      const machine = createMachine();
-      machine.updateWorkObj(obj);
-  
-      // grab a copy of the validated data object
-      const args = machine.getWorkObj();
-  
-      // begin to construct the response object
-      result.sender = args.message.From;
-      result.orgmessage = args;
-      result.reply = [];
-  
-      //retrieve data for reply array of message object.
-      getMessageArray(args, (response) => {
-        result.reply = response.slice();
-        machine.setResponse(result);
-        let newObj = machine.getWorkObj();
-        resolve(newObj);
-      });
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
-
-const getMessageArray = async (args, cb) => {
-  const msgArray = [];
-  const topic = args.classifier.topclass.toLowerCase();
-
-  const chooseObjectProperities = (obj) => {
-    for (let property in obj) {
-      if (obj instanceof Array) {
-        msgArray.push({msg: obj[getRandomInt(obj.length - 1)]});
-        return;
+  const processRequest = async (obj) => {
+    return new Promise (function(resolve, reject) {
+      try {
+        let result = {};
+    
+        // initialize object for interrogating input.
+        const machine = createMachine();
+        machine.updateWorkObj(obj);
+    
+        // grab a copy of the validated data object
+        const args = machine.getWorkObj();
+    
+        // begin to construct the response object
+        result.sender = args.message.From;
+        result.orgmessage = args;
+        result.reply = [];
+    
+        //retrieve data for reply array of message object.
+        getMessageArray(args, (response) => {
+          result.reply = response.slice();
+          machine.setResponse(result);
+          let newObj = machine.getWorkObj();
+          resolve(newObj);
+        });
+      } catch (error) {
+        reject(error);
       }
-      else if (typeof obj[property] === 'object') {
-        chooseObjectProperities(obj[property]);
-      } else {
-        msgArray.push({msg: obj[property]});
-      }
-    }
-  };
-
-  // call contenful cms to retrieve predefined content.
-  const response = await client.getEntries({
-    content_type: 'message',
-    select: 'sys.id,fields'
-  });
-
-  // TODO: come up with way of determining name of topic property dynamically
-  // filter response by the topic provided.
-  const queryResult = response.items.filter(r => r.fields.topic === topic);
-  if (queryResult.length === 0) {
-    msgArray.push({msg: 'I did not understand your request. Please contact support.'});
-  } else {
-    const fields = queryResult[0].fields;
-    // don't include the topic field in the response.
-    delete fields.topic;
-    chooseObjectProperities(fields);   
+    });
   }
-  cb(msgArray);
-};
-
-// Retrieve random number from 0 to max.
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * (max + 1));
+  
+  const getMessageArray = async (args, cb) => {
+    const msgArray = [];
+    const topic = args.classifier.topclass.toLowerCase();
+  
+    const chooseObjectProperities = (obj) => {
+      for (let property in obj) {
+        if (obj instanceof Array) {
+          msgArray.push({msg: obj[getRandomInt(obj.length - 1)]});
+          return;
+        }
+        else if (typeof obj[property] === 'object') {
+          chooseObjectProperities(obj[property]);
+        } else {
+          msgArray.push({msg: obj[property]});
+        }
+      }
+    };
+  
+    // call contenful cms to retrieve predefined content.
+    const response = await contentful.getEntries();
+  
+    // TODO: come up with way of determining name of topic property dynamically
+    // filter response by the topic provided.
+    const queryResult = response.items.filter(r => r.fields.topic === topic);
+    if (queryResult.length === 0) {
+      msgArray.push({msg: 'I did not understand your request. Please contact support.'});
+    } else {
+      const fields = queryResult[0].fields;
+      // don't include the topic field in the response.
+      delete fields.topic;
+      chooseObjectProperities(fields);   
+    }
+    cb(msgArray);
+  };
+  
+  // Retrieve random number from 0 to max.
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * (max + 1));
+  };
 };
 
 exports.startServer = main;
